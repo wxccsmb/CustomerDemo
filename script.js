5a68d21dd3c408b6997afdd60d6978d26e5ae849
wxccsmb <wxccsmb@gmail.com>
2025-09-22 14:56:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 const CUSTOMER_NAME = "Snap Diagnostics";
 
 // Use this for asset images
-const CUSTOMER_IMAGE = "Snap.png";
+const CUSTOMER_IMAGE = "Recreationalgroup.png";
 
 
 // Set this stuff once and Fuggedaboutit...
